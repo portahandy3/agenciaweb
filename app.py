9851070e4e1d3297faf004e75e18e6308ea2a5e8
HANDY KEVIN LOZANO PORTA <23LO95327648@campus.economicas.uba.ar>
2025-10-08 15:33:08 -0300

    Corrijo Flask host/port y agrego ruta raíz

--- app.py
+++ app.py
@@ -92,6 +92,11 @@ def admin():
     return render_template('admin.html', contactos=contactos)
 
 
+@app.route('/')
+def home():
+    return "🚀 Bienvenido a AgenciaWeb — Flask corriendo correctamente en Render!"
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
